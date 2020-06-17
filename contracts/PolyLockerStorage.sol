ffa6c1ba7d596efb183640525497e039495a6ddd
satyam <satyam@secureblocks.io>
2020-06-17 16:33:43 +0530

    add set method for gas variants

--- contracts/PolyLockerStorage.sol
+++ contracts/PolyLockerStorage.sol
@@ -29,17 +29,19 @@ contract PolyLockerStorage {
     // Threshold gas limit i.e 4M 
     uint256 constant public MAX_GAS_LIMIT = 4 * 10 ** 6; 
 
-    // block depth allowed (not-including the current block)
-    uint256 constant public BLOCK_DEPTH = 120; // For Aldebaran
-
-    // Threshold no. of transaction should process in last `X` blocks (i.e should be BLOCK_DEPTH)
-    uint256 constant public MAX_TXN_ALLOWED = 200; // For Aldebaran
-
     // Unit of gas increased per transaction i.e 500K
     uint256 constant public GAS_UNIT_PENALTY = 5 * 10 ** 5;
     
     // Unit of gas required to perform operations `_lock()`
     // By analyzing the older transaction it is ~ 50,376
     uint256 constant public GAS_UINT_REQUIRED_TO_LOCK = 75000; // + 20,000 for updating the txnExecutedPerBlock[block.number]  
+
+    // block depth allowed (not-including the current block)
+    // 120 For Aldebaran
+    uint256 public blockDepth;
+
+    // Threshold no. of transaction should process in last `X` blocks (i.e should be blockDepth)
+    // 200 For Aldebaran
+    uint256 public maxTxnAllowed;
 
 }
